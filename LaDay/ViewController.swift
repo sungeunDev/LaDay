fe0cdc14592cce64ea51bd91499c295d5ce86db3
ssungnni <starfg62@gmail.com>
2018-12-01 15:21:29 +0900

    Initial Commit

---
+++ LaDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaDay
+//
+//  Created by Sungeun Park on 01/12/2018.
+//  Copyright © 2018 Sungeun Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
